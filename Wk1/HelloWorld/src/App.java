f4d479791e8282aaad5a30a5f2e917875d3a167c
Scifiler-sys <pagdilaostephen@gmail.com>
2023-01-25 11:21:12 -0500

    Added today's code

--- Wk1/HelloWorld/src/App.java
+++ Wk1/HelloWorld/src/App.java
@@ -1,4 +1,5 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
 
         //A comment is designed to add additional information within your code
@@ -82,5 +83,123 @@ public class App {
 
         boolean b8 = (b3 && b4) || !(b6 || b7);
         System.out.println(b8); 
+
+        //Control Flow Statements
+        //Allows us more control over what lines needs to be run or how many times they need to be executed
+
+        //IF statements
+        //We can choose what lines to be executed based on some condition
+        if (false) {
+            //Lines between the two curly brackets are the lines that will be executed/ignored for this if statement
+            System.out.println("This specific line was executed");
+            System.out.println("More lines to be executed!");
+
+        } 
+        //Else if will only check is condition if previous if statements were false
+        else if (false) {
+            System.out.println("Else if is being executed!");
+        } else if(false){
+            System.out.println("The second else if was executed!");
+        } 
+        //else will execute if all previous if statements were false
+        else {
+            System.out.println("Else block was executed!");
+        }
+
+        //Lets put the timmy, Tom, LB scenario in code
+        double timmyHeight = 3.5;
+        double LBHeight = 6;
+        double tomHeight = 2.7;
+
+        double selectedPerson = tomHeight;
+
+        //I want a person that is between 3ft and 5ft
+        //How would you write that boolean expression?
+        if (selectedPerson >= 3 && selectedPerson <= 5) {
+            //Imaginary code that will swap the selectedPerson after the pole
+            System.out.println("This person was swapped!");
+        }
+        
+        //We can put an if like statement while assigning a value
+        // [Condition] ? [True] : [False]
+        String person = selectedPerson>= 3 ? "At least 3 feet tall" : "Not 3 feet tall";
+        System.out.println(person);
+
+        String name = "Muher";
+        //Switch statement
+        //Used to check a variable's information through multiple cases
+        //name == "Stephen"
+        switch (name) {
+            case "Stephen":
+                System.out.println("Stephen was executed");
+                break;
+            case "Munther":
+                System.out.println("Munther was executed");
+            case "Keshawn":
+                System.out.println("Keshawn was executed");
+                //Adding breaks will stop the execution of your lines of code and will not bleed over to the next case
+                break;
+            case "Jung":
+                System.out.println("Jung was executed");
+                break;
+            //Acts the same was as else block
+            default:
+                System.out.println("Random person was executed");
+                break;
+        }
+
+        //For Loops
+        //Allows you to execute the lines of code multiple times
+        //Iterate means to go to each value of an array
+
+        //Arrays
+        int[] nums = {1, 2, 3, 4, 5}; //Allows you to hardcode values within the array
+        int[] nums2 = new int[10]; //We specify the size of the array. By default, it will hold nothing but 0s
+
+        //Display all the contents of an array
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+        System.out.println(nums[4]);
+
+        //For loop that will display all the contents
+        //First block of a for loop will create temporary variables to be used within the loop
+        //Second block this is the condition the loop will check if it needs to execute again
+        //Third block, this is what will happen once a loop has finished
+        System.out.println("=========================");
+        for(int i = nums.length - 1; i>=0; i--){
+            System.out.println(nums[i]);
+        }
+
+        System.out.println("===============");
+        //Enhanced for loop
+        //You have less control on selecting specific positions
+        //TLDR if you just want to iterate through the entire array and grab its values
+        for (int i : nums)
+        {
+            System.out.println(i);
+        }
+
+        //While loops
+        //They will keep repeating the lines of code based on a condition
+        //If you don't know the exact time to stop your lines of code being executed
+        System.out.println("=============");
+        int pos = 0;
+        while (pos < nums.length) {
+            System.out.println(nums[pos++]);
+        }
+
+        boolean alwaysTrue = true;
+        //do-while loop
+        //do while will at least execute your code once regardless if the condition was false in the beginning
+        while (alwaysTrue == false) {
+            System.out.println("Will not print");
+        }
+
+        do {
+            //Will always run this code at least once
+            System.out.println("Will it print?");
+        } while (alwaysTrue == false);
     }
 }
---
+++ Wk1/QCPractice/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static double getAverage(int[] nums) {
+        //WRITE YOUR CODE HERE
+        double sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum/nums.length;
+    }
+
+    //DO NOT TOUCH ANYTHING DOWN HERE
+    //Main method is used to test your method above to check if it is working as intended
+    public static void main(String[] args) throws Exception {
+        System.out.println("Testing getAverage method...");
+        int[] nums = {1 , 2, 3, 4, 5, 6};
+
+        double result = getAverage(nums);
+        if (result == 3.5) {
+            System.out.println("\u001B[32m"+"Correct"+"\u001B[0m");
+            System.out.println("nums average is 3.5");
+        }else {
+            System.out.println("\u001B[31m"+"Failed"+"\u001B[0m");
+            System.out.println("Your method gave: " + result);
+        }
+    }
+}
