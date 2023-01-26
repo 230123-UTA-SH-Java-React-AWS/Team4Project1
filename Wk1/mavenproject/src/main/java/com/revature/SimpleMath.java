a2bc4adbe7f20e0c58418e130d1c7017c13faa01
Scifiler-sys <pagdilaostephen@gmail.com>
2023-01-26 13:41:37 -0500

    added test demo

---
+++ Wk1/mavenproject/src/main/java/com/revature/SimpleMath.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+public class SimpleMath {
+
+    //Adds two numbers given to this method
+    public int addTwoNumbers(int num1, int num2)
+    {
+        return num1+num2;
+    }
+}
