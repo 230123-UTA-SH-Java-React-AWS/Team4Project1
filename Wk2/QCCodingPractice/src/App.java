a051161dbe5ff99de2ae0ef8faab45d0b0e36bdf
Scifiler-sys <pagdilaostephen@gmail.com>
2023-01-31 14:13:56 -0500

    updated to include more test cases

--- Wk2/QCCodingPractice/src/App.java
+++ Wk2/QCCodingPractice/src/App.java
@@ -20,23 +20,30 @@ public class App {
 
     public static boolean[] determineEveryOddorEven(int nums[])
     {
-        return null;
+        return new boolean[]{};
     }
 
     public static void main(String[] args) throws Exception {
         Queue<int[]> que = new ArrayDeque<>();
 
         que.add(new int[] {1, 2, 3, 4, 5, 6});
+        que.add(new int[] {23, 432, 453, 134});
+        que.add(new int[]{});
+        que.add(new int[]{0});
+        que.add(new int[]{4});
 
         for (int[] nums : que) {
             String expected = Arrays.toString(Solution.determineEveryOddorEven(nums));
             String actual = Arrays.toString(determineEveryOddorEven(nums));
-
+            
+            System.out.println("===============================");
             if (expected.equals(actual)) {
                 System.out.println("Pass");
             }
             else 
             {
+                System.out.println("Expected - " + expected);
+                System.out.println("Actual - " + actual);
                 System.out.println("Fail");
             }
         }
