45a68ab7ee294983a3625e7c86c6904998067aae
jkof86 <jason.kofi@revature.net>
2023-02-19 21:56:27 -0500

    installed route redux and axios packages, Set up component classes for front end integration

--- reacttutorial01/src/index.jsx
+++ reacttutorial01/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+//---------------TicTacToe Tutorial---------------------------
+
 class Square extends React.Component {
   constructor(props) {
     super(props);
@@ -67,30 +69,96 @@ class Game extends React.Component {
   }
 }
 
+
+//---------------TicTacToe Tutorial---------------------------
+
+
 class Login extends React.Component {
   render() {
     return (
-      <div class="row">
-        <div class="col-sm-5">
+      <div className="row">
+        <div className="col-sm-5">
           <label>Login</label>
-          <input type="text" class="form-control" placeholder="Email" aria-label="Login" />
+          <input type="text" className="form-control" placeholder="Email" aria-label="Login" />
           <label>Password</label>
-          <input type="text" class="form-control" placeholder="" aria-label="Password" />
+          <input type="text" className="form-control" placeholder="" aria-label="Password" />
           <br />
-          <input type="button" class="btn btn-secondary btn-sm" value="Login" />
+          <input type="button" className="btn btn-secondary btn-sm" value="Login" />
         </div>
       </div>
     )
   }
 }
 
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Home</a>
+      </div>
+    )
+  }
+}
+
+class Account extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Account Info</a>
+      </div>
+    )
+  }
+}
+
+class History extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Transaction History</a>
+      </div>
+    )
+  }
+}
+
+class Transfer extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Transfer</a>
+      </div>
+    )
+  }
+}
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Register</a>
+      </div>
+    )
+  }
+}
+
+
+
 // ========================================
 //here we render the components to the index.html
-//const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
 const test1 = ReactDOM.createRoot(document.getElementById("test1"));
+const test2 = ReactDOM.createRoot(document.getElementById("test2"));
 
 //test1.render(<Game />);
 
+//On the main landing page we have 2 columns
+//column 1 is the login component
 test1.render(<Login />);
+
+//column 2 will hold the component corresponding to the link clicked on the nav bar
+ test2.render(<Home />);
+// test2.render(<Account />);
+// test2.render(<History />);
+// test2.render(<Transfer />);
+// test2.render(<Register />);
 
 
