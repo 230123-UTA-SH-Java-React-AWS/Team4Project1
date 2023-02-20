500d0cb79fab1e8441caca7fc7aab682a15103ee
Ewhitaker2023 <eugenewhitaker2022@gmail.com>
2023-02-20 17:26:18 -0600

    Add footer for image attribution

--- frontend/src/component/Footer/Footer.tsx
+++ frontend/src/component/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import navBackgroundImg from '../../resources/images/vector-jan-2021-19_generated.jpg';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-
+        <footer className='footer' style={{backgroundImage: `url(${navBackgroundImg})`}}>
+            <a href="https://www.vecteezy.com/free-vector/black-background">Black Background Vectors by Vecteezy</a>
+            <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
         </footer>
     );
 }
