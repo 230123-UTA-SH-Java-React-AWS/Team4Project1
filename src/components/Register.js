36778ffe9511ce3427420946a713bbc08a62e469
chudiercodes <chudiercodes@gmail.com>
2023-02-20 09:30:04 -0700

    registration component setup

---
+++ src/components/Register.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import axios from '../api/axios';
+import { useRef, useState, useEffect } from 'react';
+
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const USER_REGEX = /^[a-zA-Z0-9]+@+[a-zA-Z0-9]+.+[A-z]/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+// const REGISTER_URL = '/register';
+
+const Register = () => {
+	const userRef = useRef();
+	const errRef = useRef();
+
+	const [user, setUser] = useState('');
+	const [validName, setValidName] = useState(false);
+	const [userFocus, setUserFocus] = useState(false);
+
+	const [pwd, setPwd] = useState('');
+	const [validPwd, setValidPwd] = useState(false);
+	const [pwdFocus, setPwdFocus] = useState(false);
+
+	const [matchPwd, setMatchPwd] = useState('');
+	const [validMatch, setValidMatch] = useState(false);
+	const [matchFocus, setMatchFocus] = useState(false);
+
+	const [errMsg, setErrMsg] = useState('');
+	const [success, setSuccess] = useState(false);
+
+	useEffect(() => {
+		userRef.current.focus();
+	}, []);
+
+	//validate the username
+	useEffect(() => {
+		const result = USER_REGEX.test(user);
+		console.log(result);
+		console.log(user);
+		setValidName(result);
+	}, [user]);
+
+	useEffect(() => {
+		const result = PWD_REGEX.test(pwd);
+		console.log(result);
+		console.log(pwd);
+		setValidPwd(result);
+		const match = pwd === matchPwd;
+		setValidMatch(match);
+	}, [pwd, matchPwd]);
+
+	useEffect(() => {
+		setErrMsg('');
+	}, [user, pwd, matchPwd]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const data = {
+			email: user,
+			password: pwd
+		}
+
+		const contentLength = (new TextEncoder().encode(JSON.stringify(data))).length;
+        const config = {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Content-Length': contentLength
+            }
+        };
+		axios.post(`http://localhost:8000/register`, data, config)
+        .then( (res) => {
+            // dispatch(setUser(res.data));
+			console.log(res.data);
+        })
+        .catch((err) => {
+            setErrMsg("Registration failed")
+        })
+	}
+
+	return (
+		<section>
+			<p ref={errRef} className={errMsg ? "errmsg" : 
+			"offscreen"} aria-live="assertive">{errMsg}</p>
+			<h1>Register</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">
+					Email:
+					<span className={validName ? "valid" : "hide"}>
+						<FontAwesomeIcon icon={faCheck} />
+					</span>
+					<span className={validName || !user ? "hide" : "invalid"}>
+						<FontAwesomeIcon icon={faTimes} />
+					</span>
+				</label>
+				<input 
+					type="text"
+					id="username"
+					ref={userRef}
+					autoComplete="off"
+					onChange={(e)=> setUser(e.target.value)}
+					required
+					aria-invalid={validName ? "false" : "true"}
+					aria-describedby="uidnote"
+					onFocus={() => setUserFocus(true)}
+					onBlur={() => setUserFocus(false)}
+				/>
+				<p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+					<FontAwesomeIcon icon={faInfoCircle} />
+					Must be a valid email
+				</p>
+
+				<label htmlFor="password">
+					Password:
+					<span className={validPwd ? "valid" : "hide"}>
+						<FontAwesomeIcon icon={faCheck} />
+					</span>
+					<span className={validPwd || !pwd ? "hide" : "invalid"}>
+						<FontAwesomeIcon icon={faTimes} />
+					</span>
+				</label>
+				<input 
+					type="password"
+					id="password"
+					onChange={(e)=> setPwd(e.target.value)}
+					required
+					aria-invalid={validPwd ? "false" : "true"}
+					aria-describedby="pwdnote"
+					onFocus={() => setPwdFocus(true)}
+					onBlur={() => setPwdFocus(false)}
+				/>
+				<p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+					<FontAwesomeIcon icon={faInfoCircle} />
+					8 to 24 characters.<br/>
+					Must include uppercase and lowercase letters, a number and a special character.<br/>
+					Allowed special character: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+				</p>
+
+				<label htmlFor="confirm_pwd">
+					Confirm Password:
+					<span className={validMatch && matchPwd ? "valid": "hide"}>
+						<FontAwesomeIcon icon={faCheck} />
+					</span>
+					<span className={validMatch || !matchPwd ? "hide": "invalid"}>
+						<FontAwesomeIcon icon={faTimes} />
+					</span>
+				</label>
+				<input 
+					type="password"
+					id="confirm_pwd"
+					onChange={(e)=> setMatchPwd(e.target.value)}
+					value={matchPwd}
+					required
+					aria-invalid={validMatch ? "false" : "true"}
+					aria-describedby="confirmnote"
+					onFocus={() => setMatchFocus(true)}
+					onBlur={() => setMatchFocus(false)}
+				/>
+				<p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+					<FontAwesomeIcon icon={faInfoCircle} />
+					Must re-enter the password.
+				</p>
+
+				<button disabled={!validName || !validPwd || !validMatch ? true : false}> Sign Up </button>
+			</form>
+			<p>
+				Already registered? <br/>
+				<span className="line">
+					{/* put react router link here */}
+					<a href="#">Sign In</a>
+				</span>
+			</p>
+		</section>
+	)
+}
+
+export default Register
