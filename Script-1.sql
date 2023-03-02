ce53342b598fdd08da60e54f34f8a69e9b654e4a
Ewhitaker2023 <eugenewhitaker2022@gmail.com>
2023-03-02 14:57:13 -0600

    update

--- Script-1.sql
+++ Script-1.sql
@@ -1,3 +1,5 @@
+drop table transactions;
+drop table transaction_types;
 drop table accounts;
 drop table account_types;
 drop table users;
@@ -7,7 +9,7 @@ create table users (
 	fname varchar(50),
 	lname varchar(50),
 	address varchar(50),
-	username varchar(50) unique,
+	username varchar(50),
 	email varchar(50) unique,
 	pass varchar(50) not null
 );
@@ -27,7 +29,6 @@ create table accounts (
 	balance decimal default 0.00,
 	account_type integer references account_types(id) default 1
 );
-insert into accounts (user_id, account_name) values (1, 'test');
 
 create table transaction_types (
 	id integer primary key,
@@ -41,4 +42,3 @@ create table transactions (
 	amount decimal not null,
 	transaction_type integer references transaction_types(id) not null
 );
-insert into transactions (account_id, amount, transaction_type) values (1, 1.00, 1);
